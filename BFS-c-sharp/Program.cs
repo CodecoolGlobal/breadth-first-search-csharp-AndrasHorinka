191cac248ebdc8fff0cd7d8dece1c37ebc08fe08
AndrasHorinka <horinka.andras@gmail.com>
2019-05-03 11:51:31 +0200

    minimum distance between two friends

--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -6,10 +6,12 @@ namespace BFS_c_sharp
 {
     class Program
     {
+        static RandomDataGenerator generator = new RandomDataGenerator();
+        static List<UserNode> users = new List<UserNode>();
+
         static void Main(string[] args)
         {
-            RandomDataGenerator generator = new RandomDataGenerator();
-            List<UserNode> users = generator.Generate();
+            users = generator.Generate();
 
             foreach (var user in users)
             {
@@ -19,5 +21,39 @@ namespace BFS_c_sharp
             Console.WriteLine("Done");
             Console.ReadKey();
         }
+
+        public static void FriendsOfFriendsDistance(UserNode startingFriend, UserNode destinationFriend)
+        {
+            Dictionary<UserNode, int> FriendsToBeExtended = new Dictionary<UserNode, int>();
+            FriendsToBeExtended[startingFriend] = 0;
+
+            AddFriendsAtShortestDistance(startingFriend, ref FriendsToBeExtended, 0);
+
+            Console.WriteLine($"The shortest distance between {startingFriend.FirstName} and {destinationFriend.FirstName} is {FriendsToBeExtended[destinationFriend]}");
+        }
+
+        private static void AddFriendsAtShortestDistance(UserNode friend, ref Dictionary<UserNode, int> FriendsToBeExtended, int distance)
+        {
+            foreach (var f in friend.Friends)
+            {
+                if (!FriendsToBeExtended.ContainsKey(f))
+                {
+                    FriendsToBeExtended[friend] = distance + 1;
+                }
+                else
+                {
+                    FriendsToBeExtended[friend] = Math.Min(distance + 1, FriendsToBeExtended[friend]);
+                }
+
+                AddFriendsAtShortestDistance(f, ref FriendsToBeExtended, distance + 1);
+            }
+        }
     }
+
+
+
+
+}
+
+    
 }
